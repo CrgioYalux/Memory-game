f7f3129b50c8cf7a868d047657d7fa0f1fc5178a
Crgio <randaledomman1@gmail.com>
2021-02-05 13:51:53 -0300

    Fifth commit: Game is working | A better styling/feeling could be (is obviously) missing.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import Board from './components/board'
 import Difficulty from './components/difficulty'
 import Counter from './components/counter'
 
-
 function App() {
   const [difficulty, setDifficulty] = useState()
   const [visibility, setVisibility] = useState(false)
   const [isPlaying, setIsPlaying] = useState(false)
   const [time, setTime] = useState()
+  const [cantWins, setCantWins] = useState(0)
+  const [cantLoses, setCantLoses] = useState(0)
 
-  useEffect(()=>{
+  const handleWinOrLose = (setStateFunc) => {
+    setStateFunc(prev => prev + 1)
+    setVisibility(false)
+    setIsPlaying(false)
+    setTime(null)
+    setDifficulty(null)
+  }
+
+  useEffect(() => {
+      setTimeout(() => {
+        setVisibility(false)
+      }, time);
+      // return () => clearTimeout(timer)
+  }, [time])
+
+  const handleDifficulty = (difficulty) => {
+    setIsPlaying(true)
+    setVisibility(true)
+    setDifficulty(difficulty)
+
     if(difficulty === 3){
       setTime(5000) 
     }
@@ -21,28 +41,28 @@ function App() {
     else if(difficulty === 7){
       setTime(10000) 
     }
-  }, [difficulty])
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVisibility(false)
-    }, time);
-    return () => clearTimeout(timer)
-  }, [time])
-
-  const handleDifficulty = (difficulty) => {
-    setDifficulty(difficulty)
-    setIsPlaying(true)
-    setVisibility(true)
   }
 
   return (
     <>
       <div className="back"></div>
+
       <div class="flex flex-col items-center justify-center space-y-5 w-screen h-screen">
           <Difficulty handleDifficulty={handleDifficulty}/>
-          <Board difficulty={difficulty} visibility={visibility} isPlaying={isPlaying}/>
+          <Board difficulty={difficulty} visibility={visibility} isPlaying={isPlaying} handleWinning={() => handleWinOrLose(setCantWins)} handleLosing={() => handleWinOrLose(setCantLoses)}/>
           { visibility ? <Counter initialSeconds={time}/> : null }
+  
+      <div class="grid grid-cols-2 place-items-center">
+        <div class="p-4 bg-red-500 grid place-items-center rounded">
+          <h1>Loses</h1>
+          <h2>{cantLoses}</h2>
+        </div>
+        <div class="p-4 bg-green-500 grid place-items-center rounded">
+          <h1>Wins</h1>
+          <h2>{cantWins}</h2>
+        </div>
+      </div>
+      
       </div>
     </>
   );  
--- src/components/board.js
+++ src/components/board.js
@@ -1,29 +1,49 @@
+import './css/animation.css'
 import React, {useEffect, useState} from 'react'
 const generate = require('./generation')
 
-const Board = ({difficulty = 0, visibility, isPlaying}) => {
-    const [pairs, setPairs] = useState()
-    const [cantBoxes, setCantBoxes] = useState(0)
+const Board = ({difficulty = 0, visibility, isPlaying, handleWinning, handleLosing}) => {
     const [selected, setSelected] = useState([])
     const [matches, setMatches] = useState([])
-
-    useEffect(()=>{
-        setCantBoxes(difficulty)
-    },[difficulty])
+    const [pairs, setPairs] = useState([])
 
     useEffect(() => {
-        const keys = [...Array(cantBoxes**2).keys()]
-        const values = generate(cantBoxes)
-        setPairs(
-            keys.reduce((acc, key) => {
-                return [...acc, {key:key, value:values[key], active:false}]
-            }, [])
-        )
-        setSelected([])
-    }, [cantBoxes])
+        if((matches.length * 2) === (difficulty**2 - 1)) {
+            console.log("one");
+            const checks = document.querySelectorAll('.checked')
+            for(const check of checks){
+                check.classList.add('win')
+            }
+            setTimeout(() => {
+                for(const check of checks){
+                check.classList.remove('win')
+                setMatches([])
+            }}, 2000)
+
+            setTimeout(() => {
+                handleWinning()
+            }, 2001)
+        }
+    }, [matches])
+
+
+    useEffect(() => {
+        if (difficulty) {    
+            console.log("two");
+            const keys = [...Array(difficulty**2).keys()]
+            const values = generate(difficulty)
+            setPairs(
+                keys.reduce((acc, key) => {
+                    return [...acc, {key:key, value:values[key], active:false}]
+                }, [])
+                )
+                setSelected([])
+        }
+    }, [difficulty])
 
     useEffect(() => {
         if (selected.length === 2){
+            console.log("three");
             if (selected[0].value === selected[1].value){
                 setMatches(prev => {
                     return [...prev, [selected[0].key, selected[1].key]]
@@ -45,19 +65,19 @@ const Board = ({difficulty = 0, visibility, isPlaying}) => {
     }, [selected, pairs])
 
     const handleClick = (e) => {
-        if(e.target.dataset.value === "!"){
-            setSelected([])
-            setMatches([])
-            const keys = [...Array(cantBoxes**2).keys()]
-            const values = generate(cantBoxes)
-            setPairs(
-                keys.reduce((acc, key) => {
-                    return [...acc, {key:key, value:values[key], active:false}]
-                }, [])
-            )
-        }
-        else{
-            setPairs(pairs.reduce((acc, pair) => {
+            if(e.target.dataset.value === "!"){
+                e.target.classList.add('lose')
+                setTimeout(() => {
+                    setSelected([])
+                    setMatches([])
+                    e.target.classList.remove('lose')
+                }, 2000)
+                setTimeout(() => {
+                    handleLosing()
+                }, 2001)
+            }
+            else{
+                setPairs(pairs.reduce((acc, pair) => {
                     if (pair.key === Number(e.target.dataset.key)) {
                         setSelected(prev => {
                             return [...prev, {key:pair.key, value:pair.value}]
@@ -66,8 +86,9 @@ const Board = ({difficulty = 0, visibility, isPlaying}) => {
                     }
                     else return [...acc, {key:pair.key, value:pair.value, active:pair.active}]
                 }, []))
+            }
         }
-    }
+    
 
     if(isPlaying){
         return (
@@ -76,10 +97,10 @@ const Board = ({difficulty = 0, visibility, isPlaying}) => {
                 pairs.map(pair => {
                     return (
                         visibility 
-                        ? <div class="w-full h-full max-w-xs bg-red-500 hover:bg-red-400 cursor-pointer grid place-items-center text-lg font-semibold text-white" key={pair.key} data-value={pair.value}>{pair.value}</div>
+                        ? <div class="w-full h-full  bg-red-500 hover:bg-red-400 cursor-pointer grid place-items-center text-lg font-semibold text-white" key={pair.key} data-value={pair.value}>{pair.value}</div>
                         : pair.active 
-                            ? <div class="w-full h-full max-w-xs bg-yellow-300 hover:bg-red-200 cursor-pointer grid place-items-center text-lg font-semibold text-white" key={pair.key} data-key={pair.key} data-value={pair.value} data-active={pair.active}   >{pair.value}</div>
-                            : <div class="w-full h-full max-w-xs bg-red-500 hover:bg-red-400 cursor-pointer grid place-items-center text-lg font-semibold text-white" key={pair.key} data-key={pair.key} data-value={pair.value} data-active={pair.active} onClick={(e)=>{handleClick(e)}}></div>
+                            ? <div class="checked w-full h-full  bg-yellow-300 hover:bg-red-200 cursor-pointer grid place-items-center text-lg font-semibold text-white" key={pair.key} data-key={pair.key} data-value={pair.value} data-active={pair.active}>{pair.value}</div>
+                            : <div class="w-full h-full  bg-red-500 hover:bg-red-400 cursor-pointer grid place-items-center text-lg font-semibold text-white text-opacity-0" key={pair.key} data-key={pair.key} data-value={pair.value} data-active={pair.active} onClick={(e)=>{handleClick(e)}}>?</div>
                     )
                 })
             }
